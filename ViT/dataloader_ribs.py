c4ae04dce84d8ab6bae026b6ea1c16e37e61148f
jochem <kerkeruil@gmail.com>
2023-10-04 14:10:15 +0200

    test

--- ViT/dataloader_ribs.py
+++ ViT/dataloader_ribs.py
@@ -12,14 +12,14 @@ def show_slices(slices):
         axes[i].imshow(slice.T, cmap="gray", origin="lower")
     plt.show()
 
-def readin_slices(path_to_image: str, image_inds: list = None):
-    # all_paths = os.listdir(path_to_image)
-    dir_name_start = 'RibFrac'
-    dir_name_end = '-image'
-    for ind in image_inds:
-        full_path = path_to_image + dir_name_start + dir_name_end
-        print(full_path)
-        
+def readin_slices(path_to_image: str, image_inds: list = None) -> dict:
+    all_paths = os.listdir(path_to_image)
+    paths = [p for p in all_paths for ind in image_inds if ind in p]
+    
+    for p in paths:
+        pass
+
+
 
     # if image_ind:
     #     dir_name = [p for p in all_paths if image_ind in p]
@@ -29,24 +29,37 @@ def readin_slices(path_to_image: str, image_inds: list = None):
     #         dir_name = all_paths
 
     # for d in dir_name:
+
+
         
         
 
 def find_path_to_folder(tag):
+    """
+    Find folder that contains given tag.
+    Returns the local path to this folder.
+    """
     workdir = os.getcwd()
     print(f'Looking for {tag} in: {workdir}')
-    for (dir_path, dir_names, file_names) in os.walk(workdir + "/..", topdown=True):
-      if tag in dir_path:
-          break
-    print(f'Found {dir_path}\n')
-    return dir_path
+    n = len(tag)
+    found = False
+    for (dir_path, dir_names, file_names) in os.walk(workdir , topdown=True):
+    #   print(dir_path[-n:])
+      if tag == dir_path[-n:]:
+        print(f'Found {dir_path}\n')
+        found = dir_path
+        break
+
+    if not found:
+        raise Exception("Couldn't find the folder")
+    return found
 
 
     
 
 if __name__ == "__main__":
     path_to_image = find_path_to_folder('dataset')
-    readin_slices(path_to_image, ['422', '433'])
+    readin_slices(path_to_image, ['422', '423'])
 
     # im = [np.load('ViT/test.npy')]
     # im.append(np.load('ViT/test2.npy'))
